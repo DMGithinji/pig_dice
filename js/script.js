fda357eee6eab30b0b49ac13763a7015e7fb8c05
DMG <dmwaigithinji@gmail.com>
2019-05-13 09:51:09 +0300

    Add active player indicator and enable toggle once turn changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-
-//var player1=document.querySelector('#player1').textContent;
+var rolled, cummulatedScores, runningScores;
 
 cummulatedScores = [0, 0];
 runningScores = [0, 0];
 
 //random player will start
 currentPlayer = Math.floor(Math.random() * 1) + 1;
+document.getElementById('player'+currentPlayer).classList.add('active');
 
 console.log(rolled);
 
@@ -30,16 +30,24 @@ document.getElementById("rollButton").addEventListener('click', function () {
     //if rolled IS 1, switch player
     else {
         document.getElementById("running" + currentPlayer).innerHTML = 'Running Score: <span>' + 0 + '</span>';
+        document.getElementById('player'+currentPlayer).classList.remove('active');
         currentPlayer ? currentPlayer = 0 : currentPlayer = 1;
+        document.getElementById('player'+currentPlayer).classList.add('active');
     }
 });
 
 document.getElementById("holdButton").addEventListener('click', function () {
 
     //Add runningScore to cummulative score and update reading
-    cummulatedScores[currentPlayer] = +runningScores[currentPlayer];
+    cummulatedScores[currentPlayer]+=runningScores[currentPlayer];
     document.getElementById("total" + currentPlayer).innerHTML = 'Cummulated Score: <span>' + cummulatedScores[currentPlayer] + '</span>';
 
+    //update running score
+    runningScores[currentPlayer]=0;
+    document.getElementById("running" + currentPlayer).innerHTML = 'Running Score: <span>' + 0 + '</span>';
+
     //switch turn
+    document.getElementById('player'+currentPlayer).classList.remove('active');
     currentPlayer ? currentPlayer = 0 : currentPlayer = 1;
+    document.getElementById('player'+currentPlayer).classList.add('active');
 });
