d0515cbc44f33abf27e107ebff522792003ce0ec
DMG <dmwaigithinji@gmail.com>
2019-05-13 06:08:05 +0300

    Enable generation of random number and add function to enable different dice to e displayed based on generated number

--- js/script.js
+++ js/script.js
@@ -12,3 +12,14 @@ console.log(rolled);
 
 //Display the dice that's been rolled
 document.getElementById("rolled-dice").innerHTML = '<img src="./Images/Dices.png" alt="Dice"></img>';
+
+//Functions to be performed on dice roll
+document.getElementById("rollButton").addEventListener('click', function () {
+
+    //get random number between 1 - 6
+    var rolled = Math.floor(Math.random() * 6) + 1;
+    console.log(rolled);
+    //display rolled dice
+    document.getElementById("rolled-dice").innerHTML = '<img src="./Images/dice-' + rolled + '.png" alt="Dice"></img>';
+
+});
